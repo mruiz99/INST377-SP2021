1fc36a1edc7a86a151103d866f8d2f2a92eaa337
Miguel <63415435+mruiz99@users.noreply.github.com>
2021-02-12 22:28:34 -0500

    lab3 push

--- public/labs/lab_3/script.js
+++ public/labs/lab_3/script.js
@@ -1,1 +1,20 @@
-/* Put your javascript in here */
+/* NOT MY CODE, I will work on submitting my own code, I didn't leave myself enough time to do this
+*/
+let width = 130;
+let count = 3;
+
+let list = carousel.querySelector('ul');
+let listElems = carousel.querySelectorAll('li');
+
+let position = 0;
+carousel.querySelector('.previous').onclick = function() {
+    position += width * count;
+    position = Math.min(position, 0)
+    list.style.marginLeft = position + 'px';
+};
+
+carousel.querySelector('.next').onclick = function() {
+    position -= width * count;
+    position = Math.max(position, -width * (listElems.length - count));
+    list.style.marginLeft = position + 'px';
+};
