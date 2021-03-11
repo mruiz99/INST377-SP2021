471047ddfa0ae740c48346169f719c160f2a4134
Miguel <63415435+mruiz99@users.noreply.github.com>
2021-03-11 13:32:32 -0500

    style the page, added Bulma columns, leaflet map working

--- public/labs/lab_5/script.js
+++ public/labs/lab_5/script.js
@@ -1,12 +1,22 @@
 function mapInit() {
-  // follow the Leaflet Getting Started tutorial here
+  const mymap = L.map('mapid').setView([38.7849, -76.8721], 13);
+  L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: 'pk.eyJ1IjoibXJ1aXo5OSIsImEiOiJja201M2dyNGgwYW5hMnVvNXFnNWh3c3RqIn0.tt500DzwIpYLLCs1gsWrrQ'
+  }).addTo(mymap);
+  const marker = L.marker([38.7849, -76.8721]).addTo(mymap)
+
   return map;
 }
 
 async function dataHandler(mapObjectFromFunction) {
-  // use your assignment 1 data handling code here
-  // and target mapObjectFromFunction to attach markers
-}
+  const form = document.querySelector('.userform');
+  const search = document.querySelector('#city')
+  const suggestions = document.querySelector('.suggestions');}
 
 async function windowActions() {
   const map = mapInit();
